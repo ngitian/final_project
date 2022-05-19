0b869376f9019e2a0f530505489d6e492796db68
Yitian Huang <98377294+ngitian@users.noreply.github.com>
2022-05-19 10:24:35 -0700

    Add files via upload

--- step2.sh
+++ step2.sh
@@ -2,18 +2,20 @@
 
 while true; do
     outdoorTemp=$(curl --silent wttr.in/?format=%t | grep -o [0-9]*)
+    indoorTemp=boop
     echo "Outdoor: $outdoorTemp°F"
     mosquitto_pub -t "esp32/temperature" -h "192.168.1.229" -m $outdoorTemp
     mosquitto_sub -t "esp32/respond" -h "192.168.1.229" -C 1 | while read -r indoorTemp
     do
         echo "Indoor: $indoorTemp°F"
-    done
 
-    if [[ $outdoorTemp -lt $indoorTemp ]]; then
-        echo -e "Outside is hotter\n"
-    else
-        echo -e "Outside is colder\n"
-    fi
+        result=$(awk -vx=$outdoorTemp -vy=$indoorTemp 'BEGIN{print (x>=y)?1:0}')
+        if [[ $result -eq 1 ]]; then
+            echo -e "Outside is hotter\n"
+        else
+            echo -e "Outside is colder\n"
+        fi
+    done
 
     sleep 5
 done
